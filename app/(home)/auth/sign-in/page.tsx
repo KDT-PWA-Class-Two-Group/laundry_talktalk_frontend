5281e5d5a76089c190cd933b9b7d7758cd52100f
bbeagles99 <bbeagles99@gmail.com>
2025-08-20 15:01:12 +0900

    로그인 회원가입 수정

--- app/(home)/auth/sign-in/page.tsx
+++ app/(home)/auth/sign-in/page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/components/customComponents/auth/signin/LoginForm";
 
 export default function SignInPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[60vh]">
-      <h1 className="text-2xl font-bold mb-6">로그인</h1>
+    <div className="flex flex-col items-center justify-center min-h-[80vh] px-4">
+      <h1 className="text-3xl font-bold text-sky mb-6">로그인</h1>
       <LoginForm />
     </div>
   );
